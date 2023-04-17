7281d3933580ec5f7b17b833e85337d0dde2ccbb
Raksha1998 <shreerakshadesai@gmail.com>
2023-04-17 10:47:49 +0200

    Creation of a 3D vector with given components

--- Vector3D_AlgebraOperation/Vector_Lib.cs
+++ Vector3D_AlgebraOperation/Vector_Lib.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Vector3D_AlgebraOperation
 {
-    internal class Vector_Lib
+    public class Vector_Lib
     {
     }
+
+    /// <summary>
+    /// Representation of 3D Vector
+    /// </summary>
+    public struct Vector3D
+    {
+        //Components of 3D Vector
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        //constructors used to store data and validate if its constrained
+        public Vector3D(double _x, double _y, double _z)
+        {
+            X = _x;
+            Y = _y;
+            Z = _z;
+        }
+    }
+
 }
