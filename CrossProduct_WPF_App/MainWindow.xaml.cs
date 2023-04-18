bc61c3529dc940dc2bd12974640ac87a6ac06ee0
Raksha1998 <shreerakshadesai@gmail.com>
2023-04-18 14:56:59 +0200

    Created VectorViewModel.cs file, updated MainWindow.xaml and MainWindow.xaml.cs files

--- CrossProduct_WPF_App/MainWindow.xaml.cs
+++ CrossProduct_WPF_App/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CrossProduct_WPF_App.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Vector3D_AlgebraOperation;
 
 namespace CrossProduct_WPF_App
 {
@@ -20,9 +22,25 @@ namespace CrossProduct_WPF_App
     /// </summary>
     public partial class MainWindow : Window
     {
+        //
+        private VectorViewModel _view;
+        private Vector3D _viewVector1;
+        private Vector3D _viewVector2;
+
         public MainWindow()
         {
             InitializeComponent();
+            _viewVector1 = new Vector3D();
+            _viewVector2 = new Vector3D();
+            DataContext = new VectorViewModel();
+        }
+
+        string doublepattern = @"^[0-9]*(?:\.[0-9]*)?$";
+
+        private void Tbv1x1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            _viewVector1.X = Convert.ToDouble(x1.Text);
         }
     }
 }
---
+++ CrossProduct_WPF_App/ViewModel/VectorViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Vector3D_AlgebraOperation;
+
+namespace CrossProduct_WPF_App.ViewModel
+{
+    public class VectorViewModel : INotifyPropertyChanged
+    {
+        private Vector3D _vector1;
+        private Vector3D _vector2;
+        private Vector3D _CrossProduct;
+
+        public Vector3D Vector1
+        {
+            get { return _vector1; }
+            set
+            {
+                _vector1 = value;
+                OnPropertyChanged("Vector1");
+
+            }
+        }
+        public Vector3D Vector2
+        {
+            get { return _vector2; }
+            set
+            {
+                _vector2 = value;
+                OnPropertyChanged("Vector2");
+
+            }
+        }
+
+        public Vector3D CrossProduct
+        {
+            get { return _CrossProduct; }
+            set
+            {
+                _CrossProduct = value;
+                OnPropertyChanged("CrossProduct");
+
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
